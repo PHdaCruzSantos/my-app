6d4c570ec93037d445522536b1a0f4f3ce5c0965
PH da Cruz <phdacruzsantos.dev@gmail.com>
2024-01-12 14:51:34 -0300

    Create Router-Views and componet Asessments

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,15 +2,18 @@
   <div class="About">
     <h1>This is an about page</h1>
     <AboutInfos />
+    <MapaComp />
   </div>
 </template>
 
 <script>
 import AboutInfos from "../components/AboutComp/AboutComp.vue";
+import MapaComp from "../components/MapsComp/Maps.vue";
 export default {
   name: "AboutView",
   components: {
     AboutInfos,
+    MapaComp,
   },
 };
 </script>
